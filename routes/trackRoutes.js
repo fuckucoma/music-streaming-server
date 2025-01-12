cb1a376a1671cc7ee2701c520caed39b3e0298b6
fuckucoma <velkosasa21@gmail.com>
2025-01-12 16:10:53 +0300

    token

--- routes/trackRoutes.js
+++ routes/trackRoutes.js
@@ -4,6 +4,26 @@ require('dotenv').config();
 const trackController = require('../controllers/trackController');
 const { upload } = require('../middlewares/upload');
 
+const JWT_SECRET = process.env.JWT_SECRET; 
+console.log('JWT_SECRET:', process.env.JWT_SECRET);
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; 
+
+  if (!token) {
+    return res.sendStatus(401); 
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.sendStatus(403); 
+    }
+    req.user = user; 
+    next();
+  });
+};
+
 router.get('/', trackController.getTracks); 
 router.post('/add-track', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'track', maxCount: 1 }]), trackController.addTrack); 
 router.get('/:id/stream', trackController.streamTrack); 
